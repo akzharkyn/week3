8b890271db05443aaf0014043e0b633f3129b65b
akzharkyn.s <akzharkyn.s@mail.ru>
2016-02-06 09:26:07 +0600

    min max

---
+++ 2exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace _2exercise
+{
+    class Program     //2week 2nd exercise 1st method
+    {
+        static void Main(string[] args)
+        {
+            StreamReader inFile= new StreamReader(@"C:\Users\ASUS\Desktop\каз яз\input.txt"); //открывает файл инпут и читает
+            //using (StreamWriter outFile = new StreamWriter("output.txt")) //вывести на другой созданный файл
+            string ss;
+            int maxx = -1000000;
+            int minn = 10000000;
+            while ((ss=inFile.ReadLine())!=null)    //передает каждую строку в стринг и пока файл не пустой
+            {
+               
+                int a = int.Parse(ss);  //берет первое значение и парсит
+                if (maxx < a) maxx = a;
+                if (minn > a) minn = a;
+
+
+            }
+            Console.WriteLine("максимум число" + " " + maxx);
+            Console.WriteLine("минимум число" + " " + minn);
+            Console.ReadKey();
+
+
+                
+            
+
+        }
+    }
+}
---
+++ 3exercise/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _3exercise
+{
+    class Program
+    {
+        static bool findPrime(int p)
+        {
+            int cnt = 0;
+
+            for(int i=2;i<Math.Sqrt(p);i++)
+            {
+                if(p%i==0) { cnt++; }
+
+            }
+            return cnt==0 && p != 1;
+
+        }
+        static void Main(string[] args)
+        {
+            StreamReader inFile = new StreamReader(@"C:\Users\ASUS\Desktop\каз яз\numbers.txt");
+            using(StreamWriter outFile=new StreamWriter("minprime.txt"))
+            {
+                string lines = inFile.ReadLine();
+                string[] line = lines.Split(',');
+                var linetoa = new List<int>(); //list intov 
+                var atob = new List<int>();  //sostoit tolko iz intov perev so stringa
+
+                for(int i=0;i<line.Length;i++)
+                {
+                    linetoa.Add(int.Parse(line[i]));
+
+                    if(findPrime(linetoa[i])== true)
+                    {
+                        atob.Add(linetoa[i]); //zdes hranitsya uje prostye 4isla
+                    }
+
+                }
+                int min = atob[0];
+                foreach(int i in atob)  //berem kajdyi chlen massiva
+                {
+                    if(i<min) { min = i; }
+                   
+
+                }
+                outFile.WriteLine("минимум простое число это" +" " +min);
+                
+
+
+
+
+                
+                    }
+                
+                
+
+               
+
+               
+                }
+            }
+       }
+    
